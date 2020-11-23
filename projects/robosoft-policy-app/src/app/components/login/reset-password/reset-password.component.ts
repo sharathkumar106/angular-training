552470c1d1969fc3de58e1414b474978febb7bd0
sharathkumar106 <sharathkr606@gmail.com>
2020-11-23 19:56:47 +0530

    added basic password comparison

--- projects/robosoft-policy-app/src/app/components/login/reset-password/reset-password.component.ts
+++ projects/robosoft-policy-app/src/app/components/login/reset-password/reset-password.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
+  newPassword: string;
+  confirmPassword: string;
+  invalidPasswordText: string;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validatePasswordMatch(): void {
+    if (this.newPassword !== this.confirmPassword) {
+      this.invalidPasswordText = 'Passwords do not match. Please try again';
+    }
+    else {
+      this.invalidPasswordText = '';
+    }
   }
 
 }
