66b2a9c0448f3ee8455d59de354cd730a7c973e4
sharathkumar106 <sharathkr606@gmail.com>
2020-11-23 12:39:59 +0530

    Added Date Constraints to MatDatePicker

--- projects/robosoft-policy-app/src/app/components/homepage/homepage.component.ts
+++ projects/robosoft-policy-app/src/app/components/homepage/homepage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ComponentPortal } from '@angular/cdk/portal';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from './modal/modal.component';
 
@@ -10,22 +9,17 @@ import { ModalComponent } from './modal/modal.component';
 })
 export class HomepageComponent implements OnInit {
 
-  minDate: Date;
-  maxDate: Date;
   isOverlayOpen = false;
   isMenuOpen = false;
 
   constructor(public matDialog: MatDialog) {
-    // Set the minimum to January 1st 20 years in the past and December 31st 10 years in the future.
-    const currentYear = new Date().getFullYear();
-    this.minDate = new Date(currentYear - 20, 0, 1);
-    this.maxDate = new Date(currentYear + 10, 11, 31);
+
   }
 
   ngOnInit(): void {
   }
 
-  openModal(userMode: string): void {
+  openModal(): void {
     const dialogRef = this.matDialog.open(ModalComponent);
     this.isMenuOpen = false;
     dialogRef.afterClosed().subscribe(() => {
--- projects/robosoft-policy-app/src/app/components/homepage/questionnaire/questionnaire.component.ts
+++ projects/robosoft-policy-app/src/app/components/homepage/questionnaire/questionnaire.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-questionnaire',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionnaire.component.scss']
 })
 export class QuestionnaireComponent implements OnInit {
+  minDate: Date;
+  maxDate: Date;
 
-  constructor() { }
+  constructor() {
+    this.setDateConstraints();
+  }
 
   ngOnInit(): void {
+  }
+
+  // Set the minimum to January 1st 20 years in the past and December 31st 10 years in the future.
+  setDateConstraints(): void {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 20, 0, 1);
+    this.maxDate = new Date(currentYear + 10, 11, 31);
   }
 
 }
