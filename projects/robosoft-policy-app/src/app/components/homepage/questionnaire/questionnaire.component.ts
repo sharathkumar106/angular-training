0fa1cd6edd2d0fa5fd831c1e50eab6496ab49f7e
sharathkumar106 <sharathkr606@gmail.com>
2020-11-25 13:34:34 +0530

    Added Questionnaire Forms Binding

--- projects/robosoft-policy-app/src/app/components/homepage/questionnaire/questionnaire.component.ts
+++ projects/robosoft-policy-app/src/app/components/homepage/questionnaire/questionnaire.component.ts
@@ -1,3 +1,5 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Forms } from './../../../models/forms.model';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -8,11 +10,54 @@ import { Component, Input, OnInit } from '@angular/core';
 export class QuestionnaireComponent implements OnInit {
   minDate: Date;
   maxDate: Date;
-  constructor() {
+  readonly maxSize = 104857600; // 100 MB
+
+  formData: Forms = {
+    Title: '',
+    Description: 'These Guidelines helps you to code in a secure manner which would assist in keeping the product data safe',
+    ButtonTitle: 'I Accept',
+    ButtonText: 'I have read the guidelines',
+    CheckBoxText: 'I have read the guidelines',
+    StartDate: new Date(2020, 0, 1),
+    EndDate: new Date(2021, 0, 1),
+    AutoReminder: 5,
+    ContentPPT: '',
+    ParticipantsList: '',
+    MailBody: 'Hello <Name> You are hear-by invited to read and accept the terms and conditions by clicking on the following link <link>'
+  };
+  formSubmitted = false;
+
+  MainQuestionnaire: FormGroup;
+  AdditionalQuestionnaire: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
     this.setDateConstraints();
   }
 
   ngOnInit(): void {
+    this.MainQuestionnaire = this.formBuilder.group({
+      Title: [this.formData.Title, Validators.required],
+      Description: [this.formData.Description, Validators.required],
+      ButtonTitle: [this.formData.ButtonTitle, Validators.required],
+      ButtonText: [this.formData.ButtonText, Validators.required],
+      CheckBoxText: [this.formData.CheckBoxText, Validators.required],
+    });
+    this.AdditionalQuestionnaire = this.formBuilder.group({
+      StartDate: [this.formData.StartDate, Validators.required],
+      EndDate: [this.formData.EndDate, Validators.required],
+      AutoReminder: [this.formData.AutoReminder, Validators.required],
+      ContentPPT: [undefined, Validators.required, FileValidator.],
+      ParticipantsList: [this.formData.ParticipantsList],
+      MailBody: [this.formData.MailBody, Validators.required]
+    });
+  }
+
+  public get MainForm(): any {
+    return this.MainQuestionnaire.controls;
+  }
+
+  public get AdditionalForm(): any {
+    return this.AdditionalQuestionnaire.controls;
   }
 
   // Set the minimum to January 1st 20 years in the past and December 31st 10 years in the future.
@@ -20,6 +65,15 @@ export class QuestionnaireComponent implements OnInit {
     const currentYear = new Date().getFullYear();
     this.minDate = new Date(currentYear - 20, 0, 1);
     this.maxDate = new Date(currentYear + 10, 11, 31);
+  }
+
+  onSave(): void {
+    this.formSubmitted = true;
+    if (this.MainQuestionnaire.invalid || this.AdditionalQuestionnaire.invalid) {
+      console.log('Please fill all the fields!');
+    } else {
+      console.log(this.formData);
+    }
   }
 
 }
---
+++ projects/robosoft-policy-app/src/app/models/forms.model.ts
@@ -0,0 +1,13 @@
+export interface Forms {
+  Title: string;
+  Description: string;
+  ButtonTitle: string;
+  ButtonText: string;
+  CheckBoxText: string;
+  StartDate: Date;
+  EndDate: Date;
+  AutoReminder: number;
+  ContentPPT: string;
+  ParticipantsList: string;
+  MailBody: string;
+}
